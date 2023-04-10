c85791140042bd199dc69cd2978af8d465b0e96c
Nikitushkin96 <106300952+Nikitushkin96@users.noreply.github.com>
2023-04-10 20:54:51 +0300

    player36

---
+++ player36test/code/config.js
@@ -0,0 +1,21 @@
+	Config = {
+		"item": {
+			"allow360X":true,
+			"interaction": "mousemove", // "drag"
+			"impetus": "true", //"false"
+			"containerSelector": ".pro360",
+			"autoplay": {"interval": 100, "bounce": false},//frame timeout
+			"folder":"500/",
+			"zoomfolder":"2000/",
+			"file": "{XXX}.jpg", // {-XXX}.jpg for order inversion
+			//"invertScroll": true, // uncomment for inverting scroll direction
+			"type": "row",
+			"startX":1,
+			"minX": 1,
+			"maxX": 37,
+			"width": 450,
+			"height": 600,
+			"zoomfactor": 4
+		}
+
+	}
